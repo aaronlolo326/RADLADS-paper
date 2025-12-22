d8d99a75ba5d3b408968ceac27546d4df9eaa3e3
aaronlolo326 <aaronlo326@gmail.com>
2025-12-22 20:02:26 +0000

    scripts

---
+++ scripts/20251222_plot.sh
@@ -0,0 +1,4 @@
+bash /home/hei/RADLADS-paper/scripts/20251222__qwen3-8b_gdn_convF_rmsF_l2T/plot.sh
+# bash /home/hei/RADLADS-paper/scripts/20251222__qwen3-8b_gdn_convF_rmsT_l2F/plot.sh
+# bash /home/hei/RADLADS-paper/scripts/20251222__qwen3-8b_gdn_convF_rmsT_l2T_30m70m/plot.sh
+# bash /home/hei/RADLADS-paper/scripts/20251222__qwen3-8b_gdn_convF_rmsT_l2T_negEigT/plot.sh
