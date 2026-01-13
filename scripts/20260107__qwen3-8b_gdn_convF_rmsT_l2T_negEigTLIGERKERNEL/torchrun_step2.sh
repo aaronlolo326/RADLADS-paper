53fc6576d5634c365ce19144d1d05f3dac5bb9c2
Yanan <xfxcwynlc@gmail.com>
2026-01-13 03:20:44 +0000

    add 2 node torchrun multinode training commands

---
+++ scripts/20260107__qwen3-8b_gdn_convF_rmsT_l2T_negEigTLIGERKERNEL/torchrun_step2.sh
@@ -0,0 +1,22 @@
+source "$(dirname "$0")/vars.sh"
+echo "$RUN_NAME"
+
+export MASTER_ADDR=192.168.240.63
+export MASTER_PORT=29503
+export NNODES=2
+export NPROC_PER_NODE=8
+export NODE_RANK=0
+
+RWKV_TORCH_COMPILE=0 \
+RWKV_JIT_ON=0 \
+torchrun --nnodes=$NNODES --nproc_per_node=$NPROC_PER_NODE \
+  --node_rank=$NODE_RANK --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT \
+  train.py \
+    -c ${qwen_yaml} \
+    -c ${la_yaml} \
+    -c configs/${RUN_NAME}/qwen3-8b-instructteacher.yaml \
+    -c configs/${RUN_NAME}/distill3.yaml \
+    --train.load_model "/work/yanan/radlads/out/pths/20260106__qwen3-8b_gdn_convF_rmsT_l2T_negEigTshortmix2k-2/ckpt-19.pth" \
+    --train.proj_name ${RUN_NAME} \
+    --train.num_nodes 2 \
+    --train.devices $NPROC_PER_NODE
